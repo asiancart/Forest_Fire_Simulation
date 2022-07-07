4bf04a14102b9f076b019dc581a83ff1b3052ded
ilyass <ex_p@live.com>
2022-07-07 14:24:23 +0300

    Tricky Questions game

--- main.py
+++ main.py
@@ -1,186 +1,49 @@
 import random , sys
 
-BLANK = ""
+QUESTIONS = [
+    {'question': "How many times can you take 2 apples from a pile of 10 apples?", 'answer': "Once. Then you have a pile of 8 apples.",'accept': ['once', 'one', '1']},
+    {'question': 'What begins with "e" and ends with "e" but only has one letter in it?','answer': "An envelope.",'accept': ['envelope']},
+    {'question': "Is it possible to draw a square with three sides?",'answer': "Yes. All squares have three sides. They also have a fourth side.",'accept': ['yes']},
+    {'question': "What does a towel get as it dries?",'answer': "Wet.",'accept': ['wet']},
+    {'question': "Which letter of the alphabet makes honey?",'answer': "None. A bee is an insect, not a letter.",'accept': ['no', 'none', 'not']},
+    {'question': "What kind of vehicle has four wheels and flies?",'answer': "An airplane.",'accept': ['airplane', 'plane']},
+]
 
-def main():
-    print("""2048 game by asiancart""")
+CORRECT_TEXT = ['Correct!', 'That is right.', "You're right.",'You got it.', 'Righto!']
+INCORRECT_TEXT = ['Incorrect!', "Nope, that isn't it.", 'Nope.','Not quite.', 'You missed it.']
 
-    input("Press enter to begin")
-    gameBoard = getNewBoard()
-    while True:
-        drawBoard(gameBoard)
-        print("Score:",getScore(gameBoard))
-        playerMove = askForPlayerMove()
-        gameBoard = makeMove(gameBoard,playerMove)
-        addTwoToBoard(gameBoard)
-        if isFull(gameBoard):
-            drawBoard(gameBoard)
-            print("Game over - Thanks for playing")
-            sys.exit()
+print('''Trick Questions, by asiancart''')
+input('Press Enter to begin...')
+random.shuffle(QUESTIONS)
+score = 0
 
+for questionNumber, qa in enumerate(QUESTIONS):
+    print('\n' * 40)
+    print('Question:', questionNumber + 1)
+    print('Score:', score, '/', len(QUESTIONS))
+    print('QUESTION:', qa['question'])
+    response = input(' ANSWER: ').lower()
 
-def getNewBoard():
-    """Returns a new data structure that represents a board.It's a dictionary with keys of (x, y) tuples and values of the tile at that space. The tile is either a power-of-two integer or BLANK.The coordinates are laid out as:
-     X0 1 2 3
-    Y+-+-+-+-+
-    0| | | | |
-     +-+-+-+-+
-    1| | | | |
-     +-+-+-+-+
-    2| | | | |
-     +-+-+-+-+
-    3| | | | |
-     +-+-+-+-+"""
-
-    newBoard = {}
-    for x in range(4):
-        for y in range(4):
-            newBoard[(x,y)] = BLANK
-
-    startingTwosPlaced = 0
-    while startingTwosPlaced < 2:
-        randomSpace = (random.randint(0,3),random.randint(0,3))
-        if newBoard[randomSpace] == BLANK:
-            newBoard[randomSpace] = 2
-            startingTwosPlaced = startingTwosPlaced +1
-
-    return newBoard
-
-def drawBoard(board):
-    """Draws the board data structure on the screen."""
-    labels = []
-    for y in range(4):
-        for x in range(4):
-            tile = board[(x,y)]
-            labelForThisTile = str(tile).center(5)
-            labels.append(labelForThisTile)
-
-    print("""
-    +-----+-----+-----+-----+
-    |     |     |     |     |
-    |{}|{}|{}|{}|
-    |     |     |     |     |
-    +-----+-----+-----+-----+
-    |     |     |     |     |
-    |{}|{}|{}|{}|
-    |     |     |     |     |
-    +-----+-----+-----+-----+
-    |     |     |     |     |
-    |{}|{}|{}|{}|
-    |     |     |     |     |
-    +-----+-----+-----+-----+
-    |     |     |     |     |
-    |{}|{}|{}|{}|
-    |     |     |     |     |
-    +-----+-----+-----+-----+
-    """.format(*labels))
-
-def getScore(board):
-    """Returns the sum of all the tiles on the board data structure."""
-    score = 0
-    for x in range(4):
-        for y in range(4):
-            if board[(x,y)] != BLANK:
-                score = score + board[(x,y)]
-    return score
-
-def combineTilesInColumn(column):
-    """The column is a list of four tile. Index 0 is the "bottom" of the column, and tiles are pulled "down" and combine if they are the same. For example, combineTilesInColumn([2, BLANK, 2, BLANK]) returns [4, BLANK, BLANK, BLANK]."""
-
-    combinedTiles = []
-    for i in range(4):
-        if column[i] != BLANK:
-            combinedTiles.append(column[i])
-
-    while len(combinedTiles) < 4:
-        combinedTiles.append(BLANK)
-
-    for i in range(3):
-        if combinedTiles[i] == combinedTiles[i+1]:
-            combinedTiles[i] *= 2
-            for aboveIndex in range(i+1,3):
-                combinedTiles[aboveIndex] = combinedTiles[aboveIndex+1]
-            combinedTiles[3] = BLANK
-    return combinedTiles
-
-def makeMove(board,move):
-    """Carries out the move on the board.The move argument is either 'W', 'A', 'S', or 'D' and the function returns the resulting board data structure."""
-    if move == "W":
-        allColumnsSpaces =  [[(0, 0), (0, 1), (0, 2), (0, 3)],
-                             [(1, 0), (1, 1), (1, 2), (1, 3)],
-                             [(2, 0), (2, 1), (2, 2), (2, 3)],
-                             [(3, 0), (3, 1), (3, 2), (3, 3)]]
-    elif move == "A":
-        allColumnsSpaces =  [[(0, 0), (1, 0), (2, 0), (3, 0)],
-                             [(0, 1), (1, 1), (2, 1), (3, 1)],
-                             [(0, 2), (1, 2), (2, 2), (3, 2)],
-                             [(0, 3), (1, 3), (2, 3), (3, 3)]]
-    elif move == "S":
-        allColumnsSpaces =  [[(0, 3), (0, 2), (0, 1), (0, 0)],
-                             [(1, 3), (1, 2), (1, 1), (1, 0)],
-                             [(2, 3), (2, 2), (2, 1), (2, 0)],
-                             [(3, 3), (3, 2), (3, 1), (3, 0)]]
-    elif move == "D":
-        allColumnsSpaces = [[(3, 0), (2, 0), (1, 0), (0, 0)],
-                            [(3, 1), (2, 1), (1, 1), (0, 1)],
-                            [(3, 2), (2, 2), (1, 2), (0, 2)],
-                            [(3, 3), (2, 3), (1, 3), (0, 3)]]
-
-    boardAfterMove = {}
-    for columnSpaces in allColumnsSpaces:
-        firstTileSpace = columnSpaces[0]
-        secondTileSpace = columnSpaces[1]
-        thirdTileSpace = columnSpaces[2]
-        fourthTileSpace = columnSpaces[3]
-
-        firstTile = board[firstTileSpace]
-        secondTile = board[secondTileSpace]
-        thirdTile = board[thirdTileSpace]
-        fourthTile = board[fourthTileSpace]
-
-        column = [firstTile,secondTile,thirdTile,fourthTile]
-        combinedTilesColumn = combineTilesInColumn(column)
-
-        boardAfterMove[firstTileSpace] = combinedTilesColumn[0]
-        boardAfterMove[secondTileSpace] = combinedTilesColumn[1]
-        boardAfterMove[thirdTileSpace] = combinedTilesColumn[2]
-        boardAfterMove[fourthTileSpace] = combinedTilesColumn[3]
-
-    return boardAfterMove
-
-def askForPlayerMove():
-    """Asks the player for the direction of their next move (or quit). Ensures they enter a valid move: either 'W', 'A', 'S' or 'D'."""
-    print("Enter move: (WASD or Q to quit)")
-    while True:
-        move = input("> ").upper()
-        if move == "Q":
-            print("Thanks for playing")
-            sys.exit()
-        if move in ("W","A","S","D"):
-            return move
-        else:
-            print('Enter one of "W", "A", "S", "D", or "Q".')
-
-
-def addTwoToBoard(board):
-    """Adds a new 2 tile randomly to the board."""
-    while True:
-        randomSpace = (random.randint(0,3),random.randint(0,3))
-        if board[randomSpace] == BLANK:
-            board[randomSpace] = 2
-            return
-
-def isFull(board):
-    """Returns True if the board data structure has no blanks."""
-    for x in range(4):
-        for y in range(4):
-            if board[(x,y)] == BLANK:
-                return False
-    return True
-
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
+    if response == 'quit':
+        print('Thanks for playing!')
         sys.exit()
 
+    correct = False
+    for acceptanceWord in qa['accept']:
+        if acceptanceWord in response:
+            correct = True
+
+    if correct:
+        text = random.choice(CORRECT_TEXT)
+        print(text, qa['answer'])
+        score += 1
+    else:
+        text = random.choice(INCORRECT_TEXT)
+        print(text, 'The answer is:', qa['answer'])
+    response = input('Press Enter for the next question...').lower()
+
+    if response == 'quit':
+        print('Thanks for playing!')
+        sys.exit()
+
+print("That's all the questions. Thanks for playing!")
