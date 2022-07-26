d9d4207ce367b473f6d76ac2c3b746654bc036bf
ilyass <ex_p@live.com>
2022-07-26 12:28:30 +0300

    Fish Tank

--- main.py
+++ main.py
@@ -1,107 +1,231 @@
-import sys, random, time
+import random,sys,time
 
 try:
     import bext
 except ImportError:
+    print('This program requires the bext module.')
     sys.exit()
 
 WIDTH, HEIGHT = bext.size()
 WIDTH -= 1
-NUMBER_OF_LOGOS = 5
-PAUSE_AMOUNT = 0.2
-COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
-UP_RIGHT = 'ur'
-UP_LEFT = 'ul'
-DOWN_RIGHT = 'dr'
-DOWN_LEFT = 'dl'
-DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
-COLOR = 'color'
+NUM_KELP = 2
+NUM_FISH = 10
+NUM_BUBBLERS = 1
+FRAMES_PER_SECOND = 4
 
-X = 'x'
-Y = 'y'
-DIR = 'direction'
+FISH_TYPES = [
+    {"right":  ['><>'], 'left': ['<><']},
+    {'right': ['>||>'], 'left': ['<||<']},
+    {'right': ['>))>'], 'left': ['<[[<']},
+    {'right': ['>||o', '>||.'], 'left': ['o||<', '.||<']},
+    {'right': ['>))o', '>)).'], 'left': ['o[[<', '.[[<']},
+    {'right': ['>-==>'], 'left': ['<==-<']},
+    {'right': [r'>\\>'], 'left': ['<//<']},
+    {'right': ['><)))*>'], 'left': ['<*(((><']},
+    {'right': ['}-[[[*>'], 'left': ['<*]]]-{']},
+    {'right': [']-<)))b>'], 'left': ['<d(((>-[']},
+    {'right': ['><XXX*>'], 'left': ['<*XXX><']},
+    {'right': ['_.-._.-^=>', '.-._.-.^=>','-._.-._^=>', '._.-._.^=>'],
+     'left': ['<=^-._.-._', '<=^.-._.-.','<=^_.-._.-', '<=^._.-._.']},
+]
+
+LONGEST_FISH_LENGTH = 10
+LEFT_EDGE = 0
+RIGHT_EDGE = WIDTH - 1 - LONGEST_FISH_LENGTH
+TOP_EDGE = 0
+BOTTOM_EDGE = HEIGHT - 2
 
 def main():
+    global FISHES,BUBBLERS,BUBBLES,KELPS,STEP
+    bext.bg('black')
     bext.clear()
-    logos = []
-    for i in range(NUMBER_OF_LOGOS):
-        logos.append({COLOR: random.choice(COLORS),
-                    X: random.randint(1, WIDTH - 4),
-                    Y: random.randint(1, HEIGHT - 4),
-                    DIR: random.choice(DIRECTIONS)})
-        if logos[-1][X] % 2== 1:
-            logos[-1][X] -= 1
 
-    cornerBounces = 0
+    FISHES = []
+    for i in range(NUM_FISH):
+        FISHES.append(generateFish())
+
+    BUBBLERS = []
+    for i in range(NUM_BUBBLERS):
+        BUBBLERS.append(random.randint(LEFT_EDGE,RIGHT_EDGE))
+
+    BUBBLES = []
+
+    KELPS = []
+    for i in range(NUM_KELP):
+        kelpx = random.randint(LEFT_EDGE,RIGHT_EDGE)
+        kelp = {'x': kelpx, 'segments': []}
+
+        for i in range(random.randint(6,HEIGHT-1)):
+            kelp['segments'].append(random.choice(['(', ')']))
+        KELPS.append(kelp)
+
+    STEP = 1
     while True:
-        for logo in logos:
-            bext.goto(logo[X],logo[Y])
+        simulateAquarium()
+        drawAquarium()
+        time.sleep(1 / FRAMES_PER_SECOND)
+        clearAquarium()
+        STEP += 1
+
+def getRandomColor():
+    return random.choice(('black', 'red', 'green', 'yellow', 'blue','purple', 'cyan', 'white'))
+
+def generateFish():
+    fishType = random.choice(FISH_TYPES)
+
+    colorPattern = random.choice(('random', 'head-tail', 'single'))
+    fishLength = len(fishType['right'][0])
+    if colorPattern == 'random':
+        colors = []
+        for i in range(fishLength):
+            colors.append(getRandomColor())
+    if colorPattern == 'single' or colorPattern == 'head-tail':
+        colors = [getRandomColor()] * fishLength
+    if colorPattern == 'head-tail':
+        headTailColor = getRandomColor()
+        colors[0] = headTailColor
+        colors[-1] = headTailColor
+
+    fish = {'right': fishType['right'],
+            'left': fishType['left'],
+            'colors': colors,
+            'hSpeed': random.randint(1, 6),
+            'vSpeed': random.randint(5, 15),
+            'timeToHDirChange': random.randint(10, 60),
+            'timeToVDirChange': random.randint(2, 20),
+            'goingRight': random.choice([True, False]),
+            'goingDown': random.choice([True, False])}
+
+    fish['x'] = random.randint(0, WIDTH - 1 - LONGEST_FISH_LENGTH)
+    fish['y'] = random.randint(0, HEIGHT - 2)
+    return fish
+
+def simulateAquarium():
+    global FISHES, BUBBLERS, BUBBLES, KELP, STEP
+
+    for fish in FISHES:
+        if STEP % fish['hSpeed'] == 0:
+            if fish['goingRight']:
+                if fish['x'] != RIGHT_EDGE:
+                    fish['x'] += 1
+                else:
+                    fish['goingRight'] = False
+                    fish['colors'].reverse()
+            else:
+                if fish['x'] != LEFT_EDGE:
+                    fish['x'] -= 1
+                else:
+                    fish['goingRight'] = True
+                    fish['colors'].reverse()
+
+        fish['timeToHDirChange'] -= 1
+        if fish['timeToHDirChange'] == 0:
+            fish['timeToHDirChange'] = random.randint(10,60)
+            fish['goingRight'] = not fish['goingRight']
+
+        if STEP % fish['vSpeed'] == 0:
+            if fish['goingDown']:
+                if fish['y'] != BOTTOM_EDGE:
+                    fish['y'] += 1
+                else:
+                    fish['goingDown'] = False
+            else:
+                if fish['y'] != TOP_EDGE:
+                    fish['y'] -= 1
+                else:
+                    fish['goingDown'] = True
+
+        fish['timeToVDirChange'] -= 1
+        if fish['timeToVDirChange'] == 0:
+            fish['timeToVDirChange'] = random.randint(2,20)
+            fish['goingDown'] = not fish['goingDown']
+
+    for bubbler in BUBBLERS:
+        if random.randint(1,5) == 1:
+            BUBBLES.append({'x': bubbler, 'y':HEIGHT-2})
+
+    for bubble in BUBBLES:
+        diceRoll = random.randint(1,6)
+        if (diceRoll== 1) and (bubble['x'] != LEFT_EDGE):
+            bubble['x'] -= 1
+        elif (diceRoll == 2) and (bubble['x'] != RIGHT_EDGE):
+            bubble['x'] += 1
+
+        bubble['y'] -= 1
+
+    for i in range(len(BUBBLES)-1,-1,-1):
+        if BUBBLES[i]['y'] == TOP_EDGE:
+            del BUBBLES[i]
+
+    for kelp in KELPS:
+        for i, kelpSegment in enumerate(kelp['segments']):
+            if random.randint(1,20) == 1:
+                if kelpSegment == '(':
+                    kelp['segments'][i] = ')'
+                elif kelpSegment == ')':
+                    kelp['segments'][i] = '('
+
+def drawAquarium():
+    global FISHES, BUBBLERS, BUBBLES, KELP, STEP
+
+    bext.fg('white')
+    bext.goto(0,0)
+    print('Fish Tank, by asiancart')
+
+    bext.fg('white')
+    for bubble in BUBBLES:
+        bext.goto(bubble['x'], bubble['y'])
+        print(random.choice(('o', 'O')), end='')
+
+    for fish in FISHES:
+        bext.goto(fish['x'], fish['y'])
+
+        if fish['goingRight']:
+            fishText = fish['right'][STEP % len(fish['right'])]
+        else:
+            fishText = fish['left'][STEP % len(fish['left'])]
+
+        for i, fishPart in enumerate(fishText):
+            bext.fg(fish['colors'][i])
+            print(fishPart, end='')
+
+    bext.fg('green')
+    for kelp in KELPS:
+        for i, kelpSegment in enumerate(kelp['segments']):
+            if kelpSegment == '(':
+                bext.goto(kelp['x'], BOTTOM_EDGE - i)
+            elif kelpSegment == ')':
+                bext.goto(kelp['x'] + 1, BOTTOM_EDGE - i)
+            print(kelpSegment, end='')
+
+    bext.fg('yellow')
+    bext.goto(0, HEIGHT - 1)
+    print(chr(9617) * (WIDTH - 1), end='')
+
+    sys.stdout.flush()
+
+def clearAquarium():
+    global FISHES, BUBBLERS, BUBBLES, KELP
+
+    for bubble in BUBBLES:
+        bext.goto(bubble['x'], bubble['y'])
+        print(' ', end='')
+
+    for fish in FISHES:
+        bext.goto(fish['x'], fish['y'])
+
+        print(' ' * len(fish['left'][0]), end='')
+
+    for kelp in KELPS:
+        for i, kelpSegment in enumerate(kelp['segments']):
+            bext.goto(kelp['x'], HEIGHT - 2 - i)
             print(' ', end='')
 
-            originalDirection = logo[DIR]
+    sys.stdout.flush()
 
-            if logo[X] == 0 and logo[Y] == 0:
-                logo[DIR] = DOWN_RIGHT
-                cornerBounces += 1
-            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
-                logo[DIR] = UP_RIGHT
-                cornerBounces += 1
-            elif logo[X] == WIDTH-3 and logo[Y] == 0:
-                logo[DIR] = DOWN_LEFT
-                cornerBounces += 1
-            elif logo[X] == WIDTH-3 and logo[Y] == HEIGHT -1:
-                logo[DIR] = UP_LEFT
-                cornerBounces += 1
-            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
-                logo[DIR] = UP_RIGHT
-            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
-                logo[DIR] = DOWN_RIGHT
-            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
-                logo[DIR] = UP_LEFT
-            elif logo[X] == WIDTH -3 and logo[DIR] == DOWN_RIGHT:
-                logo[DIR] = DOWN_LEFT
-            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
-                logo[DIR] = DOWN_LEFT
-            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
-                logo[DIR] = DOWN_RIGHT
-            elif logo[Y] == HEIGHT-1 and logo[DIR] == DOWN_LEFT:
-                logo[DIR] = UP_LEFT
-            elif logo[Y] == HEIGHT-1 and logo[DIR] == DOWN_RIGHT:
-                logo[DIR] = UP_RIGHT
-
-            if logo[DIR] != originalDirection:
-                logo[COLOR] = random.choice(COLORS)
-
-            if logo[DIR] == UP_RIGHT:
-                logo[X] += 2
-                logo[Y] -= 1
-            elif logo[DIR] == UP_LEFT:
-                logo[X] -= 2
-                logo[Y] -= 1
-            elif logo[DIR] == DOWN_RIGHT:
-                logo[X] += 2
-                logo[Y] += 1
-            elif logo[DIR] == DOWN_LEFT:
-                logo[X] -= 2
-                logo[Y] += 1
-
-        bext.goto(5,0)
-        bext.fg("white")
-        print('Corner bounces:', cornerBounces, end='')
-
-        for logo in logos:
-            bext.goto(logo[X],logo[Y])
-            bext.fg(logo[COLOR])
-            print('DVD', end='')
-
-        bext.goto(0,0)
-        sys.stdout.flush()
-        time.sleep(PAUSE_AMOUNT)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print()
         sys.exit()
